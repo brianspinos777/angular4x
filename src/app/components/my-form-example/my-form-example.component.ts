3c8c89576ba6e7ab9ce1b6a6aa56b1e0b5d6cc93
Brian Spinos <brian@petroleumrx.com>
2017-09-25 15:49:10 -0700

    minor refactoring

--- src/app/components/my-form-example/my-form-example.component.ts
+++ src/app/components/my-form-example/my-form-example.component.ts
@@ -11,7 +11,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class MyFormExampleComponent implements OnInit {
 
-    myForm:any;
+    myForm:FormGroup;
 
     constructor(){}
 
@@ -19,23 +19,27 @@ export class MyFormExampleComponent implements OnInit {
 
         // FormGroup makes it easy to group many inputs and validate the whole form!
         // Forms built this way are called 'Reactive forms',
-        // and simple forms that are only coded on the view are called 'Template driven forms'
+        // and simple forms that are only coded on the view are called 'Template driven forms' and they use ngModel on the view
         this.myForm = new FormGroup({
-            firstName: new FormControl("", Validators.compose([
+
+            // keys here should match the 'formControlName' attributes in the form, in the view.
+
+            // 'someComplexObject': new FormGroup({}), // FormGroup is nestable!
+            'firstName': new FormControl("", Validators.compose([
                 Validators.required,
                 Validators.minLength(3),
                 Validators.pattern('[\\w\\-\\s\\/]+'),
             ])),
-            lastName: new FormControl("", this.textValidator), // custom validator
-            languages: new FormControl("", Validators.compose([
+            'lastName': new FormControl("", this.textValidator), // custom validator
+            'languages': new FormControl("", Validators.compose([
                 Validators.required,
                 Validators.minLength(3),
                 Validators.pattern('[\\w\\-\\s\\/]+'),
             ])),
-            checkbox_a: new FormControl("", Validators.compose([])),
-            checkbox_b: new FormControl("", Validators.compose([])),
-            email: new FormControl("", Validators.compose([])),
-            password: new FormControl("", Validators.compose([])),
+            'checkbox_a': new FormControl("", Validators.compose([])),
+            'checkbox_b': new FormControl("", Validators.compose([])),
+            'email': new FormControl("", Validators.compose([])),
+            'password': new FormControl("", Validators.compose([])),
         })
     }
 
