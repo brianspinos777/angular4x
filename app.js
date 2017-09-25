fe30ea50ac8f50c2ec7a64de8d66a3009c50a4b2
Brian Spinos <brian@petroleumrx.com>
2017-09-25 10:56:41 -0700

    refactor app.js to allow working faster on development, using `$ ng serve` and CORS

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
+let express = require('express');
+let app = express();
+let path = require('path');
+let bodyParser = require('body-parser');
+
+
+//------------------------------------------ Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// CORS middleware
+//
+// This way we can work on the DEVELOPMENT environment on the 
+// client-side angular app (http://localhost:4200), using `$ ng serve`, (so we can get automatic recompilation)
+// and the back-end using `$ nodemon app.js` (http://localhost:5000)
+//
+// BUT, remember to use `$ ng build --prod` before sending to Heroku.
+//
+let allowCrossDomain = (req, res, next) => {
+    let client = 'http://localhost:4200'
+    res.header('Access-Control-Allow-Origin', client); // allow 'localhost:4200' to access this API
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
+
 //------------------------------------------ postgres
 // $ npm i pg
 
@@ -11,8 +36,7 @@ var bodyParser = require('body-parser');
 // depricated:
 // https://www.youtube.com/watch?v=ijSzX3S5Qco&list=PLillGF-RfqbaEmlPcX5e_ejaK7Y5MydkW&index=3
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+
 
 
 
@@ -29,7 +53,7 @@ pool.query('SELECT NOW()', (err, res) => {
   pool.end()
 })
 
-app.get('/pg', function(req, res){
+app.get('/pg', (req, res) => {
   console.log("--------------------- PG:");
 
   const client = new Client({
@@ -49,12 +73,12 @@ app.get('/pg', function(req, res){
 
 // // // psql> $ CREATE DATABASE briandb;
 // // // psql> $ \c briandb;
-// // var connectionString = process.env.DATABASE_URL || 'postgres://username:password@localhost:5432/briandb';
+// // let connectionString = process.env.DATABASE_URL || 'postgres://username:password@localhost:5432/briandb';
 
-// // var client = new pg.Client(connectionString);
+// // let client = new pg.Client(connectionString);
 // // client.connect();
-// // var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-// // query.on('end', function(){
+// // let query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+// // query.on('end', () => {
 // // 	client.end();
 // // });
 
@@ -73,19 +97,19 @@ app.get('/pg', function(req, res){
 //------------------------------------------ mongo
 // https://github.com/mschwarzmueller/nodejs-basics-tutorial/blob/master/09-mongodb/routes/index.js
 // // npm i mongodb
-// var MongoClient = require('mongodb').MongoClient
+// let MongoClient = require('mongodb').MongoClient
  
 // // Connection URL 
-// var url = 'mongodb://localhost:27017/briandb';
+// let url = 'mongodb://localhost:27017/briandb';
 
 // // Use connect method to connect to the Server 
-// MongoClient.connect(url, function(err, db) {
+// MongoClient.connect(url, (err, db) => {
 //   console.log("-------> Connected correctly to server");
 
-//   var collection = db.collection('users');
+//   let collection = db.collection('users');
 
 //   collection.find({name: 'erich'})
-//   .toArray(function(err, docs){
+//   .toArray((err, docs) => {
 //     console.log("\n\n\n");
 //     console.log(docs);
 //     console.log("\n\n\n");
@@ -93,7 +117,7 @@ app.get('/pg', function(req, res){
 
 
 //   // collection.insert({name: 'erich', dob: new Date(1988, 0, 16)})
-//   // .then(function(res){
+//   // .then((res) => {
 //   //   console.log("\n\n\n");
 //   //   console.log(res);
 //   //   console.log("\n\n\n");
@@ -127,43 +151,43 @@ app.get('/pg', function(req, res){
 // https://github.com/mschwarzmueller/nodejs-basics-tutorial/blob/master/11-mongoose/routes/index.js
 
 // npm i mongoose
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/briandb', {
     useMongoClient: true, // this prevents some errors messages...
 });
 mongoose.Promise = global.Promise; // because mongoose's primise internal library is depricated
-var Schema = mongoose.Schema;
+let Schema = mongoose.Schema;
 
-var userSchema = new Schema({
+let userSchema = new Schema({
   name: {type: String, required: true},
   address: String,
 }, {collection: 'users'});
 
-var User = mongoose.model('User', userSchema);
+let User = mongoose.model('User', userSchema);
 
 
-app.get('/hello', function(req, res, next) {
+app.get('/hello', (req, res, next) => {
     console.log('HEY');
     res.json("HEY");
 });
 
-app.get('/getUsers', function(req, res, next) {
+app.get('/getUsers', (req, res, next) => {
   User.find()
-      .then(function(doc) {
+      .then((doc) => {
         res.json(doc);
       });
 });
 
-app.post('/insertUser', function(req, res, next) {
+app.post('/insertUser', (req, res, next) => {
     console.log('insertUser');
-  var item = {
+  let item = {
     name: "dude", //req.body.name,
     address: "foobar 123" //req.body.address
   };
 
   console.log(item);
 
-  var data = new User(item);
+  let data = new User(item);
   data.save();
 
   console.log(data);
@@ -171,10 +195,10 @@ app.post('/insertUser', function(req, res, next) {
   // res.redirect('/');
 });
 
-app.post('/updateUser', function(req, res, next) {
-  var id = '59bc1c0f22be482adcbcbe00'; //req.body.id;
+app.post('/updateUser', (req, res, next) => {
+  let id = '59bc1c0f22be482adcbcbe00'; //req.body.id;
 
-  User.findById(id, function(err, doc) {
+  User.findById(id, (err, doc) => {
     if (err) {
       console.error('error, no entry found');
     }
@@ -185,8 +209,8 @@ app.post('/updateUser', function(req, res, next) {
   res.redirect('/');
 });
 
-app.post('/deleteUser', function(req, res, next) {
-  var id = '59bc1c0f22be482adcbcbe00'; //req.body.id;
+app.post('/deleteUser', (req, res, next) => {
+  let id = '59bc1c0f22be482adcbcbe00'; //req.body.id;
   User.findByIdAndRemove(id).exec();
   res.redirect('/');
 });
@@ -202,17 +226,17 @@ app.use(express.static(__dirname + '/dist'));
 
 
 //------------------------------------------ routes
-var apiRoute = require('./server/routes/api');
+let apiRoute = require('./server/routes/api');
 app.use('/api', apiRoute)
 // curl -i http://localhost:5000/api/
 // curl -i http://localhost:5000/api/foo
 
 
 
-app.get('*', function(request, response) {
+app.get('*', (request, response) => {
   response.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
