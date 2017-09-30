a57c2842345a447c3ef5f2380ef715ffa264bef0
Brian Spinos <brian@petroleumrx.com>
2017-09-29 17:34:55 -0700

    add "page not found"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }                    from '@angular/platform-browser';
+import { NgModule }                         from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
+import { HttpModule }                       from '@angular/http';
+import { Routes, RouterModule }             from '@angular/router';
 
 // Services
-import { AuthService } from './services/auth/auth.service';
-import { ItemService } from './services/items/item.service';
+import { AuthService }                      from './services/auth/auth.service';
+import { ItemService }                      from './services/items/item.service';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { GroupsComponent } from './components/groups/groups.component';
+// Pipes
+import { MySummaryPipe }                    from './pipes/my-summary/my-summary.pipe';
 
-// Items CRUD
-import { IndexItemComponent } from './pages/items/index-item/index-item.component';
-import { ShowItemComponent } from './pages/items/show-item/show-item.component';
-import { NewItemComponent } from './pages/items/new-item/new-item.component';
-import { EditItemComponent } from './pages/items/edit-item/edit-item.component'
+// Directives
+import { MyInputFormatterDirective }        from './directives/my-input-formatter/my-input-formatter.directive';
 
 // Guards
-import { AuthGuard } from './guards/auth/auth.guard';
+import { AuthGuard }                        from './guards/auth/auth.guard';
+
+// Components
+import { MyChildComponent }                 from './components/my-child/my-child.component';
+import { MyFormExampleComponent }           from './components/my-form-example/my-form-example.component';
+import { NavbarComponent }                  from './components/navbar/navbar.component';
+import { GroupsComponent }                  from './components/groups/groups.component';
+
+
+// Pages
+import { LoginComponent }                   from './pages/login/login.component';
+import { HomeComponent }                    from './pages/home/home.component';
+import { FeaturesComponent }                from './pages/features/features.component';
+import { PageNotFoundComponent }            from './pages/page-not-found/page-not-found.component'
+
+// Items CRUD
+import { IndexItemComponent }               from './pages/items/index-item/index-item.component';
+import { ShowItemComponent }                from './pages/items/show-item/show-item.component';
+import { NewItemComponent }                 from './pages/items/new-item/new-item.component';
+import { EditItemComponent }                from './pages/items/edit-item/edit-item.component'
+
+import { AppComponent }                     from './app.component';
 
 //========================================================= REDUX
 import { NgRedux, NgReduxModule } from 'ng2-redux';
@@ -36,12 +52,8 @@ import { INITIAL_STATE, IAppState } from './redux/app.state'
 import { rootReducer } from './redux/reducers/rootReducer'
 //=========================================================
 import { appRoutes } from './routes/appRoutes';
-import { MyChildComponent } from './components/my-child/my-child.component';
-import { MyFormExampleComponent } from './components/my-form-example/my-form-example.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MyInputFormatterDirective } from './directives/my-input-formatter/my-input-formatter.directive';
-import { MySummaryPipe } from './pipes/my-summary/my-summary.pipe';
-import { FeaturesComponent } from './pages/features/features.component'
+
+
 
 
 @NgModule({
@@ -59,7 +71,8 @@ import { FeaturesComponent } from './pages/features/features.component'
         NavbarComponent,
         MyInputFormatterDirective,
         MySummaryPipe,
-        FeaturesComponent
+        FeaturesComponent,
+        PageNotFoundComponent
     ],
     imports: [
         BrowserModule,
--- src/app/routes/appRoutes.ts
+++ src/app/routes/appRoutes.ts
@@ -1,17 +1,23 @@
 import { Routes }   from '@angular/router'
 
 import { AppComponent } from '../app.component'
-import { LoginComponent } from '../pages/login/login.component'
-import { HomeComponent } from '../pages/home/home.component'
+
+// Components
 import { GroupsComponent } from '../components/groups/groups.component'
 import { MyFormExampleComponent } from '../components/my-form-example/my-form-example.component'
+
 
 // Items CRUD
 import { IndexItemComponent } from '../pages/items/index-item/index-item.component'
 import { ShowItemComponent } from '../pages/items/show-item/show-item.component'
 import { NewItemComponent } from '../pages/items/new-item/new-item.component'
 import { EditItemComponent } from '../pages/items/edit-item/edit-item.component'
+
+// Pages
 import { FeaturesComponent } from '../pages/features/features.component'
+import { PageNotFoundComponent } from '../pages/page-not-found/page-not-found.component'
+import { LoginComponent } from '../pages/login/login.component'
+import { HomeComponent } from '../pages/home/home.component'
 
 import { AuthGuard } from '../guards/auth/auth.guard';
 
@@ -70,10 +76,9 @@ export const appRoutes:Routes = [
         canActivate: [AuthGuard],
         component: EditItemComponent
     },
-
-    // {
-    //     path: '**', // the 'catch al' route!
-    //     canActivate: [AuthGuard],
-    //     component: MyPageNotFoundComponent
-    // },
+    {
+        path: '**', // the 'catch al' route!
+        canActivate: [AuthGuard],
+        component: PageNotFoundComponent
+    },
 ]
