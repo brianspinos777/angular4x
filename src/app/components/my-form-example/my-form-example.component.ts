92f5eda3cfb988f941487d73044fff4ac32ca203
Brian Spinos <brian@petroleumrx.com>
2017-10-03 12:14:02 -0700

    more fixes on forms

--- src/app/components/my-form-example/my-form-example.component.ts
+++ src/app/components/my-form-example/my-form-example.component.ts
@@ -12,19 +12,10 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class MyFormExampleComponent implements OnInit {
 
     myForm: FormGroup;
+    fb: FormBuilder;
 
     constructor(fb: FormBuilder){
-        /*
-        // FormBuilder example:
-        this.myForm = fb.group({
-            name: [initialValue, validators, asyncValidators],
-            contact: fb.group({
-                email: [initialValue, validators, asyncValidators],
-                phone: [initialValue, validators, asyncValidators]
-            }),
-            topics: fb.array([])
-        })
-        */
+        this.fb = fb;
     }
 
     ngOnInit(){
@@ -32,38 +23,45 @@ export class MyFormExampleComponent implements OnInit {
         //================================== ATTENTION =========================
         //
         // FormBuilder : there is a way to write less code with 'Reactive forms', use `FormBuilder`
+        this.useFormBuilder(this.fb);
         //
         //======================================================================
 
         // FormGroup makes it easy to group many inputs and validate the whole form!
-        // Forms built this way are called 'Reactive forms',
-        // and simple forms that are only coded on the view are called 'Template driven forms' and they use ngModel on the view
-        this.myForm = new FormGroup({
+        // 'Reactive forms' => Forms built this way are called 'Reactive forms',
+        // 'Template driven forms' => and simple forms that are only coded on the view are called 'Template driven forms' and they use ngModel on the view
+        
+        // this.myForm = new FormGroup({
 
-            // keys here should match the 'formControlName' attributes in the form, in the view.
+        //     // keys here should match the 'formControlName' attributes in the form, in the view.
 
-            // 'someComplexObject': new FormGroup({}), // FormGroup is nestable! remember to use 'formGroupName' in the template though.
-            'firstName': new FormControl('', Validators.compose([
-                Validators.required,
-                Validators.minLength(3),
-                Validators.pattern('[\\w\\-\\s\\/]+'),
-            ])),
-            'lastName': new FormControl('', this.textValidator), // custom validator
-            'languages': new FormControl('', Validators.compose([
-                Validators.required,
-                Validators.minLength(3),
-                Validators.pattern('[\\w\\-\\s\\/]+'),
-            ])),
-            'checkbox_a': new FormControl('', Validators.compose([])),
-            'checkbox_b': new FormControl('', Validators.compose([])),
-            'email': new FormControl('', Validators.compose([])),
-            'password': new FormControl('', Validators.compose([])),
-        });
+        //     // 'someComplexObject': new FormGroup({}), // FormGroup is nestable! remember to use 'formGroupName' in the template though.
+        //     'firstName': new FormControl('', Validators.compose([
+        //         Validators.required,
+        //         Validators.minLength(3),
+        //         Validators.pattern('[a-zA-Z]+'),
+        //     ])),
+        //     'lastName': new FormControl('', this.textValidator), // custom validator
+        //     'languages': new FormControl('', Validators.compose([
+        //         Validators.required,
+        //         Validators.minLength(3),
+        //         Validators.pattern('[a-zA-Z]+'),
+        //     ])),
+        //     'checkbox_a': new FormControl('', Validators.compose([])),
+        //     'checkbox_b': new FormControl('', Validators.compose([])),
+        //     'email': new FormControl('', Validators.compose([])),
+        //     'password': new FormControl('', Validators.compose([])),
+        // });
     }
 
     textValidator(control){
-        if (control.value.length < 3){
-            return {'lastName': true};
+        if (control.value.length < 6){
+            return {
+                'lastName': true, // myForm.get('lastName').errors.lastName
+                'myMinlength': {
+                    myRequiredLength: 6 // myForm.get('lastName').errors.myMinlength.myRequiredLength
+                }
+            };
         }
     }
 
@@ -77,5 +75,56 @@ export class MyFormExampleComponent implements OnInit {
         console.log(data);
     }
 
+    useFormBuilder(fb: FormBuilder){
+
+        /*
+        // FormBuilder example:
+        this.myForm = fb.group({
+            name: [initialValue, validators, asyncValidators],
+            contact: fb.group({
+                email: [initialValue, validators, asyncValidators],
+                phone: [initialValue, validators, asyncValidators]
+            }),
+            topics: fb.array([])
+        })
+        */
+
+        this.myForm = fb.group({
+            // keys here should match the 'formControlName' attributes in the form, in the view.
+
+            // 'someComplexObject': fb.group({}), // fb.group({}) is nestable! remember to use 'formGroupName' in the template though.
+
+            'firstName': ['Brian', [
+                Validators.required,
+                Validators.minLength(3),
+                Validators.pattern('[a-zA-Z]+')
+            ], []],
+            'lastName': ['', [
+                Validators.required,
+                // Validators.minLength(3),
+                Validators.pattern('[a-zA-Z]+'),
+                this.textValidator // custom validator
+            ], []],
+            'languages': ['', [
+                Validators.required
+            ], []],
+            'checkbox_a': ['', [
+                Validators.required
+            ], []],
+            'checkbox_b': ['', [
+                Validators.required
+            ], []],
+            'email': ['', [
+                Validators.required
+            ], []],
+            'password': ['', [
+                Validators.required
+            ], []],
+        }) 
+    }
+
+    submitForm2(formObj){
+        console.log(formObj.value);
+    }
 
 }
