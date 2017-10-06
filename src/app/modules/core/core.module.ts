65868ec6feae786578f2156d2edd1d3d7d4c246f
Brian Spinos <brian@petroleumrx.com>
2017-10-06 15:00:08 -0700

    added shared and core modules

---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// ATTENTION: this module is intended for providers only (services, guards?)
+//            DO NOT add declarations here!
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class CoreModule { }
---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// ATTENTION: 
+//    - this module is intended for declarations only (components, directives and pipes)
+//    - The declarations here have access to global providers!     
+//    - DO NOT add providers here!
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class SharedModule { }
